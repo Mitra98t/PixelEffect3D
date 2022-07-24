c1f978d806515bebb5782e13deecf1b89304f59b
Mitra98t <personalmailfm98@gmail.com>
2022-07-24 09:30:15 +0200

    Casting Shadows done

--- Assets/ToonShad.hlsl
+++ Assets/ToonShad.hlsl
@@ -17,16 +17,31 @@ struct Mods{
 
 float Toon(float3 normal, Light l, Mods mm){
     float NdotL = max(0.0f,(dot(normalize(normal), normalize(l.direction)) + mm.Offset));
-    
+    // SHADOW ATTENUATION CAUSA ARTEFATTI A CAZZO DI CANE
+    //* TOON SHADE ATTENUATION USING SHADOW AND ADDITIONAL LIGHT (if provided)
+    float attenuation = l.shadowAttenuation * l.distanceAttenuation;
+    NdotL *= attenuation;
+
     return floor(NdotL / mm.Detail) == 0 ? floor(NdotL / mm.Detail) + mm.DarkestShadow : floor(NdotL / mm.Detail);
+}
+float3 CalculateToonShading(float diffuse, Light l, Mods mm){
+    return l.color * diffuse * mm.Strength + mm.Brightness;
 }
 #endif
 
-void LightingCelShaded_float(float3 Normal,float4 ColorIn, float Brightness, float Strength, float Detail, float Offset, float DarkestShadow, out float3 Color){
+void LightingCelShaded_float(float3 Normal, float3 Position, float4 ColorIn, float Brightness, float Strength, float Detail, float Offset, float DarkestShadow, out float3 Color){
 #if defined(SHADERGRAPH_PREVIEW)
     Color = float3(0.3f,0.3f,0.3f);
 #else
-    Light light = GetMainLight();
+    //* MAIN LIGHT SHADOW COORDINATES
+    #if SHADOWS_SCREEN
+        float4 clipPos = TransformWorldToClip(Position);
+        float4 ShadowCoord = ComputeScreenPos(clipPos);
+    #else
+        float4 ShadowCoord = TransformWorldToShadowCoord(Position);
+    #endif
+
+    Light light = GetMainLight(ShadowCoord);
     Mods mm;
     
     mm.Brightness = Brightness;
@@ -38,7 +53,15 @@ void LightingCelShaded_float(float3 Normal,float4 ColorIn, float Brightness, flo
 
     Color = mm.Color;
 
-    Color *= light.color * Toon(Normal, light, mm) * mm.Strength + mm.Brightness;
+    Color *= CalculateToonShading(Toon(Normal, light, mm), light, mm);
+
+    //* ADDITIONAL LIGHTS CALCULATION
+    // int pixelLightCount = GetAdditionalLightsCount();
+    // for(int i = 0; i < pixelLightCount; i++){
+    //     light = GetAdditionalLight(i, Position, 1);
+    //     float3 tmpColor = CalculateToonShading(Toon(Normal, light, mm), light, mm);
+    //     Color *= tmpColor;
+    // }
 #endif
 }
 #endif
